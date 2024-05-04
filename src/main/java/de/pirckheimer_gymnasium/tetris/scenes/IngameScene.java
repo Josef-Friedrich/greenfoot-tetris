03bacf223d3c3329f8a55b244e9a66ba4e93fb93
Josef Friedrich <josef@friedrich.rocks>
2024-05-04 11:56:01 +0200

    IngameScene

--- src/main/java/de/pirckheimer_gymnasium/tetris/scenes/IngameScene.java
+++ src/main/java/de/pirckheimer_gymnasium/tetris/scenes/IngameScene.java
@@ -12,37 +12,67 @@ public class IngameScene extends BaseScene implements KeyListener
 {
     private BlockGrid grid;
 
+    /**
+     * Der Zufallsgenerator wird benötigt, um zufällig neue Tetrominos zu
+     * erzeugen. Wir verwenden die Method nextInt() um zufällig Zahlen von 0 bis
+     * einschließlich 6 zu bekommen.
+     *
+     * @see #createNextTetromino()
+     */
     private static Random random = new Random();
 
+    /**
+     * Die Nummer des nächsten Tetrominos.
+     */
     private int nextTetromino;
 
+    /**
+     * Das aktuelle Tetromino, das gesteuert werden kann und automatisch nach
+     * unten fällt.
+     */
     private Tetromino tetromino;
+
+    /**
+     * Das Vorschaubild des nächsten Tetrominos im linken unteren Bereich.
+     */
+    private Tetromino previewTetromino;
 
     public IngameScene()
     {
         super("ingame");
         // Das I-Tetromino ragt einen Block über das sichtbare Spielfeld hinaus,
-        // wenn es in der Startposition gedreht wird.
+        // wenn es in der Startposition gedreht wird, deshalb machen wir das
+        // Blockgitter um eine Zeile höher.
         grid = new BlockGrid(Tetris.GRID_WIDTH, Tetris.HEIGHT + 1);
-        setNextTetrominoNo();
-        createTetromino();
+        createNextTetromino();
         repeat(1, () -> {
             if (!tetromino.moveDown())
             {
-                createTetromino();
+                createNextTetromino();
             }
         });
     }
 
-    private void setNextTetrominoNo()
+    private void createNextTetromino()
     {
+        // Beim ersten Mal müssen zwei zufällige Tetrominos erzeugt werden.
+        // Wir müssen also zweimal eine Zufallszahl generieren.
+        if (previewTetromino == null)
+        {
+            nextTetromino = random.nextInt(7);
+        }
+        tetromino = Tetromino.create(this, grid, nextTetromino, 4, 16,
+                Tetris.DEBUG);
         nextTetromino = random.nextInt(7);
-    }
-
-    private void createTetromino()
-    {
-        Tetromino.create(this, grid, nextTetromino, 3, 0, Tetris.DEBUG);
-        setNextTetrominoNo();
+        // Entfernen des alten Vorschaubildes, falls vorhanden.
+        if (previewTetromino != null)
+        {
+            previewTetromino.remove();
+        }
+        // Das Vorschaubild liegth außerhalb des Blockgitters. Wir übergeben der
+        // Methode null.
+        previewTetromino = Tetromino.create(this, null, nextTetromino, 14, 3,
+                Tetris.DEBUG);
     }
 
     @Override
