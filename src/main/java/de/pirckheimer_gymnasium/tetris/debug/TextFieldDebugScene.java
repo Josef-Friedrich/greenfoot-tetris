f4ee18f33f442bed380553a26df80c3b3892a44e
Josef Friedrich <josef@friedrich.rocks>
2024-05-19 21:41:31 +0100

    Refactor

--- src/main/java/de/pirckheimer_gymnasium/tetris/debug/TextFieldDebugScene.java
+++ src/main/java/de/pirckheimer_gymnasium/tetris/debug/TextFieldDebugScene.java
@@ -1,32 +1,20 @@
 package de.pirckheimer_gymnasium.tetris.debug;
 
 import java.awt.Color;
-import java.awt.event.KeyEvent;
 
 import de.pirckheimer_gymnasium.tetris.Tetris;
 import de.pirckheimer_gymnasium.tetris.text.TextField;
 import rocks.friedrich.engine_omega.Scene;
-import rocks.friedrich.engine_omega.event.KeyListener;
 
-public class TextFieldDebugScene extends Scene implements KeyListener
+public class TextFieldDebugScene extends Scene
 {
     public TextFieldDebugScene()
     {
         TextField textField = new TextField(this, 0, 8, 5, 20);
         getCamera().setPostion(10, 8);
-
-        textField.write("Hello, World. Lorem ipsum. Lorem ipsum. Lorem ipsum","#ff0000");
+        textField.write("Hello, World. Lorem ipsum. Lorem ipsum. Lorem ipsum",
+                "#ff0000");
         setBackgroundColor(Color.GRAY);
-    }
-
-    @Override
-    public void onKeyDown(KeyEvent keyEvent)
-    {
-        switch (keyEvent.getKeyCode())
-        {
-        case KeyEvent.VK_L:
-            break;
-        }
     }
 
     public static void main(String[] args)
