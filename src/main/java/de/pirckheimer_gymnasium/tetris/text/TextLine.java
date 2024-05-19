2e0ba562e3bf11f8b7603139a9421d44c4d0423c
Josef Friedrich <josef@friedrich.rocks>
2024-05-19 09:56:10 +0100

    TextField

--- src/main/java/de/pirckheimer_gymnasium/tetris/text/TextLine.java
+++ src/main/java/de/pirckheimer_gymnasium/tetris/text/TextLine.java
@@ -6,7 +6,7 @@ import rocks.friedrich.engine_omega.Scene;
  * Ein rechteckiges Feld, in das mit den typischen Tetris-Buchstaben geschrieben
  * werden kann.
  */
-public class TextField
+public class TextLine
 {
     /**
      * Eine Referenz auf die {@link Scene Szene}, in der das Textfeld angezeigt
@@ -27,11 +27,6 @@ public class TextField
     private int y;
 
     /**
-     * Die Anzahl an Zeilen, die das Textfeld maximal enthalten darf.
-     */
-    private int lines;
-
-    /**
      * Die Anzahl an Buchstaben, die in eine Zeile passen.
      */
     private int width;
@@ -39,7 +34,7 @@ public class TextField
     /**
      * Ein zweidimensionales Feld, das als Speicher für die Buchstaben dient.
      */
-    private Glyph[][] glyphs;
+    private Glyph[] glyphs;
 
     /**
      *
@@ -49,36 +44,27 @@ public class TextField
      *              das geschrieben werden soll.
      * @param y     Die y-Koordinate des linken oberen Ecks des Textfelds, in
      *              das geschrieben werden soll.
-     * @param lines Die Anzahl an Zeilen, die das Textfeld maximal enthalten
-     *              darf.
      * @param width Die Anzahl an Buchstaben, die in eine Zeile passen.
      */
-    public TextField(Scene scene, int x, int y, int lines, int width)
+    public TextLine(Scene scene, int x, int y, int width)
     {
         this.scene = scene;
         this.x = x;
         this.y = y;
         this.width = width;
-        this.lines = lines;
-        glyphs = new Glyph[lines][width];
+        glyphs = new Glyph[width];
     }
 
     public void write(String text, String color)
     {
         text = text.toUpperCase();
         int glyphIndex = 0;
-        int lineIndex = 0;
         for (int i = 0; i < text.length(); i++)
         {
-            glyphs[lineIndex][glyphIndex] = new Glyph(this.scene,
-                    text.charAt(i), color, x + glyphIndex, y - lineIndex);
+            glyphs[glyphIndex] = new Glyph(this.scene,
+                    text.charAt(i), color, x + glyphIndex, y);
             glyphIndex++;
             if (glyphIndex > width - 1)
-            {
-                glyphIndex = 0;
-                lineIndex++;
-            }
-            if (lineIndex > lines - 1)
             {
                 throw new RuntimeException(
                         "Der Text passt nicht in das Textfeld");
@@ -86,14 +72,10 @@ public class TextField
         }
     }
 
-    public void writeLine(String text, String color, int y) {
-
-
-    }
 
     public void clearLine(int lineIndex)
     {
-        for (Glyph glyph : glyphs[lineIndex])
+        for (Glyph glyph : glyphs)
         {
             if (glyph != null)
             {
